6f4eca5cbcdbafdc968eb6cb9b81aa79d33b283f
Anfisa Golovanova <anfisa.faa@gmail.com>
2024-06-12 02:23:55 +0700

    Initial commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+
+    // метод calculate, который:
+    // 1. принимает на вход один параметр: цену билета, типа int;
+    // 2.анализируя значение переданного параметра, возвращает рассчитанное количество миль.
+    int miles;
+
+    public int calculate(int price) {
+
+        miles = price / 20;
+
+        return miles;
+    }
 }
