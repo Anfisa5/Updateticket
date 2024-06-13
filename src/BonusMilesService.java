b05c2e02254ddae9105240839d6fabb26463ce93
Anfisa Golovanova <anfisa.faa@gmail.com>
2024-06-14 05:33:38 +0700

    updated BonusMilesService

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -3,12 +3,14 @@ public class BonusMilesService {
     // метод calculate, который:
     // 1. принимает на вход один параметр: цену билета, типа int;
     // 2.анализируя значение переданного параметра, возвращает рассчитанное количество миль.
-    int miles;
 
+    //int miles; не нужно создавать ячейку, т.к. это не локальная переменная, тк они создаются только внутри методов
     public int calculate(int price) {
 
-        miles = price / 20;
+        // int miles = price / 20; можно сразу сделать return price / 20;
 
-        return miles;
+        return price / 20;
+
+        //return miles;
     }
 }
